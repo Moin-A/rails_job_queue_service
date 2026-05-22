776e906d33a7663e1fc82a3d6a530718f239a65c
Moin Ahmed <moinahmedptw@gmail.com>
2026-05-22 17:58:14 +0530

    Add mutex and ConditionVariable-based sleep and stop to WorkerPool

--- app/lib/worker_pool.rb
+++ app/lib/worker_pool.rb
@@ -1,13 +1,23 @@
 class WorkerPool
   def initialize(concurrency: JobQueue.config.concurrency)
     @concurrency = concurrency
-    @threads = []
+    @threads     = []
+    @running     = false
+    @mutex       = Mutex.new
+    @cond        = ConditionVariable.new
   end
 
   def start
-    @concurrency.times do
-      @threads << Thread.new { }
+    @running = true
+
+    @concurrency.times do |i|
+      @threads << Thread.new { work_loop(i + 1) }
     end
+  end
+
+  def stop
+    @running = false
+    @mutex.synchronize { @cond.broadcast }
   end
 
   def thread_count
@@ -15,6 +25,18 @@ class WorkerPool
   end
 
   private
+
+  def work_loop(thread_id)
+    while @running
+      job = claim_job
+
+      if job
+        process(job, thread_id)
+      else
+        @mutex.synchronize { @cond.wait(@mutex, JobQueue.config.poll_interval) }
+      end
+    end
+  end
 
   def claim_job
     ActiveRecord::Base.transaction do
